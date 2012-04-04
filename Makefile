29ea692ae4a6ef99c3aaca2ebbb29e6452424f82
Jiawei Li <jiawei.h.li@gmail.com>
2012-04-04 00:15:27 -0500

    coffee-izing test javascript

--- Makefile
+++ Makefile
@@ -3,7 +3,9 @@ DIST = dist/
 NAME = jstile.js
 
 build:
+	coffee -cb test/init.coffee
 	coffee -j $(DIST)$(NAME) -cb $(SRC)
 
 clean:
+	rm -f test/init.js
 	rm -rf $(DIST) 
--- test/jstilewrapper.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  var placeholderSource = 'http://placehold.it/' + $(document).width() + 'x' + $(document).height();
-
-  $('div').append($('<img src=http://placehold.it/' + $(document).width() + 'x' + $(document).height() + '>'));
-
-  base = $('img').jstile();
-});
